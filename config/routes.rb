0280da8572ea51d7b28c0fff7fc46c73168cdb76
Nina Klemencic <nina4klemencic@gmail.com>
2018-01-02 15:26:37 -0800

    7 actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
+  # root is the homepage, whatever we want to display (/ = always the route)
   root 'kitties#index'
+
+  # list all kitties
   get '/kitties' => 'kitties#index'
+
+  # display a form to save a kitty
   get '/kitties/new' => 'kitties#new'
+
+  # display details for a specific kitty
   get '/kitties/:id' => 'kitties#show'
+
+  # save kitty to database
+  post '/kitties' => 'kitties#create'
+
+  # display form to edit kitty info
+  get '/kitties/:id/edit' => 'kitties#edit'
+
+  # save edited kitty info to database
+  patch '/kitties/:id' => 'kitties#update'
+
+  # remove kitty info from database
+  delete 'kitties/:id' => 'kitties#destroy'
 end
